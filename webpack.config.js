5836c49217fe4380ac3c37af4e8141638f1f73cb
sindhura-kaparthi <sindhura.kaparthi@thoughtworks.com>
2022-10-28 11:51:02 +0530

    Serving the UI

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 const cssLoader = {
@@ -17,9 +17,9 @@ const cssLoader = {
           return `${name}__${localName}`
           //
         }
-      }
-    }
-  }
+      },
+    },
+  },
 }
 
 const config = {
@@ -27,41 +27,48 @@ const config = {
   entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle/bundle.js',
   },
   target: 'web',
   devServer: {
-    port: 3030 // you can change the port
+    port: 3030, // you can change the port
   },
   module: {
     rules: [
       {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        generator: {
+          filename: './fonts/[name][ext]',
+        },
+      },
+
+      {
         use: 'swc-loader',
         test: /\.ts|tsx$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/,
         use: ['style-loader', cssLoader, 'sass-loader'],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: ['style-loader', cssLoader],
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'public/index.html' // to import index.html file inside index.js
-    })
+      template: 'public/index.html', // to import index.html file inside index.js
+    }),
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss', '.css']
-  }
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss', '.css'],
+  },
 }
 
 module.exports = config
